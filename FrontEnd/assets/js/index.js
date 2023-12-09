f8d34382349c3fa013ab09fd9f140a4ab9cb2108
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-10 00:19:47 +0100

    added a forEach loop for works + retrieved gallery section dom element that will host works

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -13,7 +13,11 @@ await getWorks();
 async function generateWorks() {
   // store http api response in a constant in a JSON format
   const works = await getWorks();
-  console.log(works);
+  // console.log(works);
+  works.forEach(work => {
+    // Retrieve the DOM element that will host the works
+    const sectionGallery = document.querySelector('.gallery');
+  });
 }
 
 generateWorks();
