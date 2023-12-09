4b9547d7ba3e4c45ad718a9b907a4e48425da513
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-09 23:52:09 +0100

    called the API with fetch to dynamically retrieve works

---
+++ FrontEnd/assets/js/index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @returns function that retrieves works from the API
+ */
+
+async function getWorks() {
+  const response = await fetch('http://localhost:5678/api/works');
+  return await response.json();
+}
+// console.log(await getWorks());
+await getWorks();
