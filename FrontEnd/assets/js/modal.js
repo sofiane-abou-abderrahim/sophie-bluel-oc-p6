4a93dd660fe77a5704a87aae3e61e58a423570bf
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-13 00:57:08 +0100

    implemented code to close modal box upon click

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -2,6 +2,9 @@
 
 const modalTriggers = document.querySelectorAll('.modal-trigger');
 
+// variable to find out which modal is opened
+let modal = null;
+
 // function to open modal
 const openModal = function (event) {
   event.preventDefault();
@@ -11,6 +14,30 @@ const openModal = function (event) {
   target.style.display = null;
   target.removeAttribute('aria-hidden');
   target.setAttribute('aria-modal', 'true');
+  // save the modal box that is currently opened
+  modal = target;
+  // close modal upon click
+  modal.addEventListener('click', closeModal);
+  modal.querySelector('.close-modal').addEventListener('click', closeModal);
+};
+
+// function to close modal
+const closeModal = function (event) {
+  // to not do anything if we try to close non existant modal
+  if (modal === null) {
+    return;
+  }
+  event.preventDefault();
+  // hide modal
+  modal.style.display = 'none';
+  modal.setAttribute('aria-hidden', 'true');
+  modal.removeAttribute('aria-modal');
+  // delete event listener
+  modal.removeEventListener('click', closeModal);
+  // delete event listener to entirely clean modal box
+  modal.querySelector('.close-modal').removeEventListener('click', closeModal);
+  // set again modal to null as by default
+  modal = null;
 };
 
 // open modal upon click on the edit link
