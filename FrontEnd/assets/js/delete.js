84b7bb3a4400a38d3c262e1b7f633c2414358fc0
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-15 18:43:17 +0100

    sent delete request to delete project upon click

--- FrontEnd/assets/js/delete.js
+++ FrontEnd/assets/js/delete.js
@@ -1,8 +1,27 @@
 //*********Deletion of existing works*********//
 
-function deleteWorks(event) {
+import { displayWorks } from './index.js';
+
+async function deleteWorks(event) {
+  // retrieve the value of the 'data-id' attribute of the HTML element that triggered the event (event)
   let id = event.target.dataset.id;
-  console.log(id);
+  // console.log(id);
+
+  // send DELETE request to the API
+  const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: '*/*',
+      Authorization: 'Bearer ' + localStorage.user
+    }
+  });
+
+  if (response.ok) {
+    event.target.parentElement.remove();
+  } else if (response.status == '401') {
+    alert('Session expirée, merci de vous reconnecter');
+    document.location.href = 'login.html';
+  }
 }
 
 export function deleteProject() {
