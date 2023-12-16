df233eeea0cecb90a751aa02290d9f5a0b4b5ff2
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-12-16 15:08:47 +0100

    improved code with more comments

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,7 +1,8 @@
 //**********Global variables*********//
 
 /**
- * @global define global scope variables
+ * Global scope variable to store the portfolio element.
+ * @type {HTMLElement}
  */
 
 // define portfolio in the global scope
@@ -10,22 +11,25 @@ const portfolio = document.getElementById('portfolio');
 //**********Function to retrieve the gallery from the API**********//
 
 /**
- * @async function that fetches data from the API and subsequently creates a gallery based on that data
+ * Async function that fetches data from the API and creates a gallery based on that data.
  */
 
 const getWorks = async () => {
   let galleryList = [];
 
   try {
-    // retrieve tasks
+    // Fetch tasks from the API
     const worksResponse = await fetch('http://localhost:5678/api/works');
     const worksData = await worksResponse.json();
     galleryList = worksData;
 
-    // call the functions after retrieving the data
+    // Log the fetched data for debugging purposes
+    console.log(galleryList);
+
+    // Call the function to create the gallery after retrieving the data
     createGallery(galleryList); // will later create HTML elements based on this data.
   } catch (error) {
-    // handle error
+    // Handle errors and log to the console
     console.error("Une erreur s'est produite : ", error);
   }
 };
@@ -33,28 +37,42 @@ const getWorks = async () => {
 //**********Function to create the gallery**********//
 
 /**
- *
- * @param {*} galleryList function that creates the gallery
+ * Function to create the gallery based on the provided data.
+ * @param {Array} galleryList - An array of objects representing gallery items.
  */
 
 const createGallery = galleryList => {
   const gallery = document.createElement('div');
   gallery.classList.add('gallery');
 
-  // iterate through each item in galleryList & generate an HTML string for each item
+  // Log the created gallery for debugging purposes
+  console.log(gallery);
+  console.log(galleryList);
+
+  /*
+    Iterate through each item in galleryList & generate an HTML string for each item.
+    The callback function takes an "el" object from "galleryList" and returns an HTML string.
+    This operation is performed for each item in the array, resulting in an array of HTML strings.
+  */
   gallery.innerHTML = galleryList
     .map(
-      img =>
+      el =>
         `<figure>
-    <img src="${img.imageUrl}" alt="${img.title}">
-    <figcaption>${img.title}</figcaption>
+    <img src="${el.imageUrl}" alt="${el.title}">
+    <figcaption>${el.title}</figcaption>
   </figure>
   `
     )
-    .join(''); // The HTML strings are joined together into a single string using .join('')
+    .join('');
+  /*
+    The .join method is used to concatenate the array of HTML strings into a single string.
+    Without .join(''), the result of the .map method would be an array of HTML strings,
+    and when assigned to gallery.innerHTML, it would insert commas between the HTML strings, creating an invalid HTML structure.
+  */
   portfolio.appendChild(gallery);
 };
 
+// Initiate the process by calling getWorks
 getWorks();
 
 // /**
